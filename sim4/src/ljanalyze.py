fcf22b70de52eb0d31bbdfcebbc0ce316ca52d8a
Michael Marquardt <st143244@stud.uni-stuttgart.de>
2018-01-08 09:39:25 +0100

    Finished Programming

--- sim4/src/ljanalyze.py
+++ sim4/src/ljanalyze.py
@@ -22,7 +22,7 @@ parser.add_argument( '--linreg', type=int, nargs='+', help='--linreg Min Max: ar
 
 args = parser.parse_args()
 
-### READING DATA-FILE #############################################################################
+### READING DATA-FILE ############################################################################
 
 if not args.ID:
     print('ERROR: simuation ID missing.\nUse --ID <ID>')
@@ -36,7 +36,8 @@ datafile = open(datafilename, 'rb')
 step, t, x, v, ts, Tms, xs, vs = pickle.load(datafile)
 datafile.close()
 
-xs = array(xs)
+xs = array(xs)[:,:,0]
+vs = array(vs)[:,:,0]
 N = shape(xs)[0]
 d = shape(xs)[1]
 if args.dtmax:
@@ -48,9 +49,9 @@ if args.linreg:
 else:
     linreg = [0,dtmax]
 
-### CALCULATE MSD #################################################################################
+### CALCULATE MSD ################################################################################
 
-def calc_msd( ndt, x=xs):
+def calc_msd( ndt, x ):
     '''
     calculates the msd for time steps of dt = ts[ndt] - ts[0]
     '''
@@ -58,26 +59,43 @@ def calc_msd( ndt, x=xs):
     k = 1
     msd = 0.
     while k < n:
-        msd += sum( ( x[ndt*k,:,0] - x[ndt*(k-1),:,0] )**2 )
+        msd += sum( ( x[ndt*k,:] - x[ndt*(k-1),:] )**2 )
         k += 1
-    return msd / (k-1)  #TODO errorbars
+    return msd / (k-1.) 
     
+def calc_err( ndt, msd, x ):
+    '''
+    calculates the error of msd_vec
+    '''
+    n = shape(x)[0]/ndt
+    k = 1
+    err = 0.
+    while k < n:
+        err += ( sum( ( x[ndt*k,:] - x[ndt*(k-1),:] )**2 ) - msd )**2
+        k += 1
+    if k > 2:
+        return sqrt( err / ( (k-1.)*(k-2.) ) )
+    else:
+        return 0.
+    
+print('Calculating MSD...')
 msd_vec = zeros(dtmax)
 dt = zeros(dtmax)
+err_vec = zeros(dtmax)
 for k in range(1,dtmax+1):
     msd_vec[k-1] = calc_msd( k, xs )
     dt[k-1] = ts[k]-ts[0]
+    err_vec[k-1] = calc_err( k, msd_vec[k-1], xs )
 
-# Plot
-plot(dt,msd_vec,'o')
+### FIT ON DATA ##################################################################################
 
-### FIT ON DATA ###################################################################################
-
+print('Fit onto MSD...')
 p = polyfit( dt[linreg[0]:linreg[1]], msd_vec[linreg[0]:linreg[1]], 1 )
-D = p[0] * 0.5 / float(d)
-print('D = {}'.format(D))
+Dx = p[0] * 0.5 / float(d)
 
 # Plot
+print('Plot MSD...')
+errorbar(dt,msd_vec,yerr=err_vec,fmt='*')
 x = linspace(linreg[0],linreg[1],1000)
 plot( x, p[0]*x+p[1], '-r')
 xlabel("Duration $\Delta t$")
@@ -85,10 +103,42 @@ ylabel("MSD of $x$")
 savefig('../dat/{}_MSD.png'.format(simulation_id))
 close()
 
-### VACF ##########################################################################################
+print('Finished MSD.')
 
-vs=array(vs)
-#TODO expand for VACF
+### VACF #########################################################################################
+
+def autocor(v):
+    '''
+    returns the autocorrelation function of vestorial observable v
+    It uses scalar produkt of v with itself
+    The result is normed to autocor(v)[0] = 1
+    '''
+    ftv = real( fft.ifft( conj( fft.fft(v, axis=0) ) * fft.fft(v, axis=0), axis=0 ) )
+    while size(ftv.shape) > 1:
+         ftv = sum( ftv, axis=1 ) 
+    return ftv[:int(ftv.shape[0]/2)] / ftv[0]
+
+ 
+# Calculate VACF  
+print('Calculate VACF...')
+vacf = autocor(vs)
+
+print('Plot VACF...')
+plot(arange(0,vacf.shape[0]),vacf)
+xlabel("VACF$")
+ylabel("Time t")
+savefig('../dat/{}_VACF.png'.format(simulation_id))
+close()
+
+# Integrate VACF
+print('Integrate VACF...')
+Dv = trapz(vacf)
+
+print('Finished VACF.')
+
+print('\nThe diffusion coefficients are:')
+print('Dx = {}'.format(Dx))
+print('Dv = {}'.format(Dv))
 
     
 
